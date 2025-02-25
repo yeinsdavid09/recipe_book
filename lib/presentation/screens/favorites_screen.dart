e51399427cd3475bc79b6bef68dc6f84e3cac9c8
Yeins David Llano Hernandez <yeinsdavid09@gmail.com>
2025-02-24 20:21:18 -0500

    feat/(screens) create favorites screen

---
+++ lib/presentation/screens/favorites_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  //#region --------------------------------- Hooks ---------------------------------
+
+  const FavoritesScreen({super.key});
+
+  //#endregion
+
+  //#region --------------------------------- Methods ---------------------------------
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  //#endregion
+}
